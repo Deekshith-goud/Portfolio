de5cd20e7a24562a0e879b93648726144daeb0cd
Deekshith-Goud <deekshithgoud7101@gmail.com>
2026-06-23 00:25:19 +0530

    feat(pages): add font assets, app icons, and animations

---
+++ app/animation/Slide.tsx
@@ -0,0 +1,42 @@
+// @ts-nocheck
+"use client";
+import { motion, useInView, useAnimation, AnimationProps } from "framer-motion";
+import { useRef, useEffect, RefObject } from "react";
+
+interface SlideProps extends AnimationProps {
+  children: React.ReactNode;
+  delay?: number;
+  className?: string;
+}
+
+export const Slide = ({ children, className, delay }: SlideProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInview = useInView(ref as RefObject<Element>, { once: true });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isInview) {
+      controls.start("stop");
+    }
+  }, [controls, isInview]);
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={{
+        start: { opacity: 0, translateY: 15, filter: "blur(8px)" },
+        stop: { opacity: 1, translateY: 0, filter: "blur(0px)" },
+      }}
+      transition={{
+        type: "spring",
+        damping: 20,
+        stiffness: 100,
+        delay: delay,
+      }}
+      animate={controls}
+      initial="start"
+    >
+      <div className={className}>{children}</div>
+    </motion.div>
+  );
+};
